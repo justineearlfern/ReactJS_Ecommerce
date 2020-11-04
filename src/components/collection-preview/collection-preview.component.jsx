ba149696c3537672b37aaace72acf6c3c71c5359
justineearlfern <fernandez1302885@ceu.edu.ph>
2020-11-04 15:03:48 +0800

    moved our directory and shop data into own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import './collection-preview.styles.scss';
+
 import CollectionItem from '../collection-item/collection-item.component';
 
+import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
@@ -12,7 +13,7 @@ const CollectionPreview = ({ title, items }) => (
         .map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div> 
+    </div>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
-ReactDOM.render(
-  <Provider store={store}>
-  <BrowserRouter>
-  <App />
-</BrowserRouter>
-</Provider>,
-  document.getElementById('root')
+import { store, persistor } from './redux/store';
+
+
+
+import './index.css';
+import App from './App';
+
+ReactDOM.render( <
+    Provider store = { store } >
+    <
+    BrowserRouter >
+    <
+    PersistGate persistor = { persistor } >
+    <
+    App / >
+    <
+    /PersistGate> < /
+    BrowserRouter > <
+    /Provider>,
+    document.getElementById('root')
 );
-
-
- 
